6d30ae75ef22108e1f365b43f4e5e17f644ed807
Morten Hyllekilde Andersen <hyllekilde88@gmail.com>
2014-09-26 12:53:05 +0200

    5.3.1-3 done

--- Ex5/TestDownload.java
+++ Ex5/TestDownload.java
@@ -5,40 +5,84 @@ import java.net.URL;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 
 public class TestDownload {
 
-  private static final String[] urls = 
-  { "http://www.itu.dk", "http://www.di.ku.dk", "http://www.miele.de",
-    "http://www.microsoft.com", "http://www.amazon.com", "http://www.dr.dk",
-    "http://www.vg.no", "http://www.tv2.dk", "http://www.google.com",
-    "http://www.ing.dk", "http://www.dtu.dk", "http://www.eb.dk", 
-    "http://www.nytimes.com", "http://www.guardian.co.uk", "http://www.lemonde.fr",   
-    "http://www.welt.de", "http://www.dn.se", "http://www.heise.de", "http://www.wsj.com", 
-    "http://www.bbc.co.uk", "http://www.dsb.dk", "http://www.bmw.com", "https://www.cia.gov" 
-  };
+	private static final String[] urls = 
+	{ "http://www.itu.dk", "http://www.di.ku.dk", "http://www.miele.de",
+		"http://www.microsoft.com", "http://www.amazon.com", "http://www.dr.dk",
+		"http://www.vg.no", "http://www.tv2.dk", "http://www.google.com",
+		"http://www.ing.dk", "http://www.dtu.dk", "http://www.eb.dk", 
+		"http://www.nytimes.com", "http://www.guardian.co.uk", "http://www.lemonde.fr",   
+		"http://www.welt.de", "http://www.dn.se", "http://www.heise.de", "http://www.wsj.com", 
+		"http://www.bbc.co.uk", "http://www.dsb.dk", "http://www.bmw.com", "https://www.cia.gov" 
+	};
+	private static final ExecutorService executor = Executors.newWorkStealingPool();
 
-  public static void main(String[] args) throws IOException {
-    String url = "http://www.wikipedia.org/";
-    String page = getPage(url, 10);
-    System.out.printf("%-30s%n%s%n", url, page);
-  }
+	public static void main(String[] args) throws IOException {
+		Timer t = new Timer();
+		Map<String, String> pages = getPages(urls, 200);
+		System.out.println(t.check());
 
-  public static String getPage(String url, int maxLines) throws IOException {
-    // This will close the streams after use (JLS 8 para 14.20.3):
-    try (BufferedReader in 
-         = new BufferedReader(new InputStreamReader(new URL(url).openStream()))) {
-      StringBuilder sb = new StringBuilder();
-      for (int i=0; i<maxLines; i++) {
-        String inputLine = in.readLine();
-        if (inputLine == null)
-          break;
-        else
-          sb.append(inputLine).append("\n");
-      }
-      return sb.toString();
-    }
-  }
+		for(Map.Entry<String, String> entry : pages.entrySet()){
+			System.out.printf("%-25s %6d\n", entry.getKey(), entry.getValue().length());
+		}
+	}
+
+	public static Map<String, String> getPagesParallel(String[] urls, int maxLines) throws IOException {
+		List<Callable<Map<String, String>>> tasks = new ArrayList<Callable<Map<String, String>>>();
+		Map<String, String> result = new HashMap<String,String>(urls.length);
+		
+		for (int t=0; t<urls.length; t++) {
+		  tasks.add(new Map<String, String>() { public Map<String, String> call() { 
+			return getPages(new String[urls[t]],maxLines);
+		  }});
+		}
+		
+		try {
+			Map<String, String> temp;
+	      	List<Future<Map<String, String>>> futures = executor.invokeAll(tasks);
+	  		for (Future<Map<String, String>> fut : futures) {
+	  			temp = fut.get();
+	    		result.put(temp.getKey(), temp.getValue());
+	    	}
+	    }
+	     catch (InterruptedException exn) { 
+	      System.out.println("Interrupted: " + exn);
+	    }
+	}
+
+	public static Map<String, String> getPages(String[] urls, int maxLines) throws IOException {
+		Map<String, String> map = new HashMap<String,String>(urls.length);
+		for(int i = 0; i<urls.length; ++i){
+			map.put(urls[i], getPage(urls[i], maxLines));
+		}
+		return Collections.unmodifiableMap(map);
+	}
+
+	public static String getPage(String url, int maxLines) throws IOException {
+		// This will close the streams after use (JLS 8 para 14.20.3):
+		try (BufferedReader in 
+				 = new BufferedReader(new InputStreamReader(new URL(url).openStream()))) {
+			StringBuilder sb = new StringBuilder();
+			for (int i=0; i<maxLines; i++) {
+				String inputLine = in.readLine();
+				if (inputLine == null)
+					break;
+				else
+					sb.append(inputLine).append("\n");
+			}
+			return sb.toString();
+		}
+	}
 }
 
---
+++ Ex5/ex5.md
@@ -0,0 +1,97 @@
+Exercise 5.1
+============
+
+Question 1
+----------
+
+
+Question 2
+----------
+
+
+Question 3
+----------
+5.1.3.2 (using newWorkStealingPool) CountParTask1 : http://goo.gl/lCRAvu
+5.1.3.2 (using newWorkStealingPool) CountParTask2 : http://goo.gl/r1bOl2
+
+Question 4
+----------
+
+
+Question 5
+----------
+
+
+Exercise 5.2
+============
+
+Question 1
+----------
+
+
+Question 2
+----------
+
+
+Question 3
+----------
+
+
+Question 4
+----------
+
+
+Question 5
+----------
+
+
+Exercise 5.3
+============
+
+Question 1
+----------
+Runs:
+1: 12.933446358
+2: 11.08861516
+3: 8.806998804
+4: 11.117534792
+5: 7.94627111
+
+Question 2
+----------
+
+
+Question 3
+----------
+
+
+Question 4
+----------
+
+
+Question 5
+----------
+
+
+Exercise 5.4
+============
+
+Question 1
+----------
+
+
+Question 2
+----------
+
+
+Question 3
+----------
+
+
+Question 4
+----------
+
+
+Question 5
+----------
+
