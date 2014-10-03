d40220e8cd2bd1d918f0ffa4636e5f069644adfa
Mikkel Riise Lund <lund.mikkel@gmail.com>
2014-10-03 16:12:40 +0200

    Histogram2

--- Ex6/TestHistogram2.java
+++ Ex6/TestHistogram2.java
@@ -1,11 +1,13 @@
 import javax.annotation.concurrent.GuardedBy;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 interface Histogram {
     public void increment(int item);
     public int getCount(int item);
     public int[] getBuckets();
     public int getSpan();
+    public void addAll(Histogram hist);
 }
 
 class Histogram2 implements Histogram {
@@ -30,5 +32,36 @@ class Histogram2 implements Histogram {
     
     public int getSpan() {
         return counts.length;
+    }
+
+    /**
+     * Avoids deadlocks by never having the lock on two objects at the same time
+     */
+    public void addAll(Histogram that) {
+        if (this.getSpan() != that.getSpan())
+            throw new RuntimeException("The histograms must be of equal length.");
+
+        // Locks on that while getting the buckets
+        final int[] buckets = that.getBuckets();
+        // No longer holds the lock on that
+
+        // Locks on this while setting the buckets
+        synchronized (this) {
+            for (int i = 0; i < counts.length; ++i)
+                counts[i] += buckets[i];
+        }
+        // No longer holds the lock on this
+    }
+
+    public void madAll(Histogram2 that) {
+        if (this.getSpan() != that.getSpan())
+            throw new RuntimeException("The histograms must be of equal length.");
+
+        // Locks on this while setting the buckets
+        synchronized (this) {
+            for (int i = 0; i < counts.length; ++i)
+                this.counts[i] += that.counts[i];
+        }
+        // No longer holds the lock on this
     }
 }
