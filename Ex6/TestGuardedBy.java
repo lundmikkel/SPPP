0eb862f2fdbe1583429be2478f6bd480c93ce3f8
Mikkel Riise Lund <lund.mikkel@gmail.com>
2014-10-03 10:26:05 +0200

    Adds threadsafe-project file

--- Ex6/TestGuardedBy.java
+++ Ex6/TestGuardedBy.java
@@ -3,6 +3,6 @@ import javax.annotation.concurrent.GuardedBy;
 class LongCounter {
     @GuardedBy("this")
     private long count = 0;
-    public synchronized void increment() { count++; }
+    public void increment() { count++; }
     public synchronized long get() { return count; }
 }
